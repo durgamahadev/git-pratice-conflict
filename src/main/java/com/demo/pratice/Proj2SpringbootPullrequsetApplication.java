0b3f99a71b42712d8e3541a605bfa46b00f51345
Durgamahadev <durgamahadevparida@gmail.com>
2021-01-27 18:14:03 +0530

    intial commit

---
+++ src/main/java/com/demo/pratice/Proj2SpringbootPullrequsetApplication.java
@@ -0,0 +1,15 @@
+package com.demo.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj2SpringbootPullrequsetApplication {
+
+	public static void main(String[] args) {
+		//FOR BRANCH : MASTER
+		ConfigurableApplicationContext context = SpringApplication.run(Proj2SpringbootPullrequsetApplication.class, args);
+		
+	}
+
+}
