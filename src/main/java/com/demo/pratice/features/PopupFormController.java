ae0d13d79fde486ca055229aa4a5fb37d0dc6844
Durgamahadev <durgamahadevparida@gmail.com>
2021-01-27 19:18:29 +0530

    commit for feature-102

---
+++ src/main/java/com/demo/pratice/features/PopupFormController.java
@@ -0,0 +1,10 @@
+package com.demo.pratice.features;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.*;
+public class  PopupFormController{
+//TODO:added for feature-102
+private static final Long SessionId=new Random().getLong();
+
+}
